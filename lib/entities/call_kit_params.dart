a9f9731c4c4bd411592390688263f4e1c9b5fc9a
yash <yzala999@gmail.com>
2024-06-14 12:41:16 +0530

    added call back for decline on terminated state

--- lib/entities/call_kit_params.dart
+++ lib/entities/call_kit_params.dart
@@ -9,24 +9,24 @@ part 'call_kit_params.g.dart';
 /// Object config for General.
 @JsonSerializable(explicitToJson: true)
 class CallKitParams {
-  const CallKitParams({
-    this.id,
-    this.nameCaller,
-    this.appName,
-    this.avatar,
-    this.handle,
-    this.type,
-    this.normalHandle,
-    this.duration,
-    this.textAccept,
-    this.textDecline,
-    this.missedCallNotification,
-    this.callingNotification,
-    this.extra,
-    this.headers,
-    this.android,
-    this.ios,
-  });
+  const CallKitParams(
+      {this.id,
+      this.nameCaller,
+      this.appName,
+      this.avatar,
+      this.handle,
+      this.type,
+      this.normalHandle,
+      this.duration,
+      this.textAccept,
+      this.textDecline,
+      this.missedCallNotification,
+      this.callingNotification,
+      this.extra,
+      this.headers,
+      this.android,
+      this.ios,
+      this.onDecline});
 
   final String? id;
   final String? nameCaller;
@@ -44,6 +44,9 @@ class CallKitParams {
   final Map<String, dynamic>? headers;
   final AndroidParams? android;
   final IOSParams? ios;
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  final Function(String reason)? onDecline;
 
   factory CallKitParams.fromJson(Map<String, dynamic> json) =>
       _$CallKitParamsFromJson(json);

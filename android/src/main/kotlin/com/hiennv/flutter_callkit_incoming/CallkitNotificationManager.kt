9a6832d9c2205eecd24d53d26710b1a047d6a436
yash <yzala999@gmail.com>
2025-05-26 16:12:10 +0530

    [FIX] Log issue on callkit kt file

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
@@ -41,6 +41,7 @@ import com.squareup.picasso.Picasso
 import com.squareup.picasso.Target
 import okhttp3.OkHttpClient
 import java.lang.reflect.Method
+import android.util.Log
 
 
 class CallkitNotificationManager(private val context: Context) {

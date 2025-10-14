949409560e837853367beec1a0e1fd58095c1cc0
yash <yzala999@gmail.com>
2025-10-14 15:55:31 +0530

    [FIX] log unresolved

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/FlutterCallkitIncomingPlugin.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/FlutterCallkitIncomingPlugin.kt
@@ -15,6 +15,8 @@ import io.flutter.plugin.common.*
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import java.lang.ref.WeakReference
+import android.util.Log
+
 
 
 /** FlutterCallkitIncomingPlugin */
